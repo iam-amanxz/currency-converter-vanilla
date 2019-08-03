3d53de67c9ee4631b261c51ef9eddc57b6e9d9ef
qrhyme <iam.amanxz@gmail.com>
2019-08-03 16:53:31 +0530

    styles applied

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,17 @@
-const currency = document.querySelector("#currency");
+const from = document.querySelector(".from__value");
+const to = document.querySelector(".to__value");
 const btn = document.getElementById("getCurrency");
-  
+
 function refreshPage() {
-    window.location.reload();
+  window.location.reload();
 }
-  
+
 btn.addEventListener("click", e => {
-  
-  let value = document.querySelector("#value")
+  let value = document.querySelector("#value");
   let fromCurrency = document.getElementById("fromCurrency");
   let toCurrency = document.getElementById("toCurrency");
 
-  if(value.value === ""){
+  if (value.value === "") {
     value.value = 1;
   }
 
@@ -19,25 +19,27 @@ btn.addEventListener("click", e => {
   fromCurrency = fromCurrency.options[fromCurrency.selectedIndex].value;
   toCurrency = toCurrency.options[toCurrency.selectedIndex].value;
   const data = {
-      value:value,
-      fromCurrency: fromCurrency,
-      toCurrency: toCurrency
+    value: value,
+    fromCurrency: fromCurrency,
+    toCurrency: toCurrency
   };
   // calls the API with POST method with data in it
   fetch("http://localhost:3000/currency", {
-      method: 'POST',
-      mode: 'cors',
-      headers: {
-          'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data)
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
   }).then(response => {
-      response.json().then(data => {
-          if (data.error) {
-            console.log(data.error);
-          } else {
-            currency.textContent = value + fromCurrency + " = " + value*(data.body) + " " + toCurrency;
-          }
-      });
+    response.json().then(data => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        from.textContent = value + " " + fromCurrency;
+        to.textContent =
+          value * parseFloat(data.body).toFixed(2) + " " + toCurrency;
+      }
+    });
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,59 +1,67 @@
-var request = require('request');
+var request = require("request");
 const path = require("path");
 const express = require("express");
-const cors = require('cors')
+const cors = require("cors");
 const hbs = require("hbs");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+// setting up releative path
 const pathPublic = path.join(__dirname, "../public");
+// customizing view path
 const pathView = path.join(__dirname, "../templates/views");
+// setting up path for partials
+const partialsPath = path.join(__dirname, "../templates/partials");
+// setting up handlebars view engine
 app.set("view engine", "hbs");
+// setting up view path
 app.set("views", pathView);
-
+// setting up path for partials
+hbs.registerPartials(partialsPath);
+// setting root route to serve public directory
+app.use(express.static(pathPublic));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(express.static(pathPublic));
-app.use(cors())
-
+app.use(cors());
 
 app.get("", (req, res) => {
   res.render("home", {
     title: "Currency Converter"
-  });  
-});   
+  });
+});
 
 app.post("/currency", (req, res) => {
-    console.log(req.body);
-    const uri = "https://currency-exchange.p.rapidapi.com/exchange?",
-    headers={
-        'x-rapidapi-host': 'currency-exchange.p.rapidapi.com',
-        'x-rapidapi-key': 'b13c4f3d67msh8143a7f1298de7bp1e8586jsn4453f885a4e7'
-    }
-    const queryObject  = {
-        q: req.body.value,
-        from: req.body.fromCurrency,
-        to: req.body.toCurrency
+  console.log(req.body);
+  const uri = "https://currency-exchange.p.rapidapi.com/exchange?",
+    headers = {
+      "x-rapidapi-host": "currency-exchange.p.rapidapi.com",
+      "x-rapidapi-key": "b13c4f3d67msh8143a7f1298de7bp1e8586jsn4453f885a4e7"
     };
-    console.log(queryObject);
-    request({
-        url:uri,
-        qs:queryObject,
-        headers: headers
-    }, function (error, response, body) {
-        if (error) {
-            console.log('error:', error); // Print the error if one occurred
-
-        } else if(response && body) {
-            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-            res.json({'body': body}); // Print JSON response.
-        }
-        })
-    });
+  const queryObject = {
+    q: req.body.value,
+    from: req.body.fromCurrency,
+    to: req.body.toCurrency
+  };
+  console.log(queryObject);
+  request(
+    {
+      url: uri,
+      qs: queryObject,
+      headers: headers
+    },
+    function(error, response, body) {
+      if (error) {
+        console.log("error:", error); // Print the error if one occurred
+      } else if (response && body) {
+        console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+        res.json({ body: body }); // Print JSON response.
+      }
+    }
+  );
+});
 app.listen(port, () => {
-console.log("Server is running on port " + port);
+  console.log("Server is running on port " + port);
 });
